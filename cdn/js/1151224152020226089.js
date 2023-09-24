35a4e8e0b0c63cdc7e84297ec8054ab2a8dbab18
Listcords <145923464+Listcords@users.noreply.github.com>
2023-09-24 19:20:52 +0300

    Add files via upload

---
+++ cdn/js/1151224152020226089.js
@@ -0,0 +1,5 @@
+
+
+let shortDescription = "Listcords, the best listing service for your Discord assets, servers and bots.";
+
+document.getElementById("mini-1151224152020226089").innerHTML = shortDescription;
